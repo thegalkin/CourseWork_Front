2321ac3da16d131a20b6c5569187aee59ffd20c3
Nik Galkin <53794828+thegalkin@users.noreply.github.com>
2021-05-03 23:44:05 +0300

    Initial Commit

---
+++ CourseWork_Front/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CourseWork_Front
+//
+//  Created by Никита Галкин on 03.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
